4b6fd04ee0b5f2212a39c16e9114a286118e9159
Denys <102409171+denxiik@users.noreply.github.com>
2025-10-10 13:18:41 +0200

    Correct tag

--- script/gtag-config.js
+++
@@ -1,9 +0,0 @@
-// /script/gtag-config.js
-// This creates the global gtag function and initializes the tracking.
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-// REPLACE 'G-PCLS8DS737' with your actual Measurement ID
-gtag('config', 'G-PCLS8DS737');
